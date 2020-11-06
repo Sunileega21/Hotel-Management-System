d37a38d2766d704a969765889d888a69e24f451c
sunil <sunil@localhost.localdomain>
2020-11-06 13:31:37 -0600

    Uploaded Code for Hotel Management System

---
+++ src/I3/DatabaseOperation/DataBaseConnection.java
@@ -0,0 +1,27 @@
+package I3.DatabaseOperation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import org.sqlite.*;
+
+
+
+
+
+public class DataBaseConnection {
+    public static Connection connectTODB()
+    {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:hotel.sqlite");
+            return conn;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.toString());
+            return null;
+        }
+        
+        
+    }
+    
+}
